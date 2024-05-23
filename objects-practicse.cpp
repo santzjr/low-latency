1c99cc42014596c6acd07c7e2a41de0d198b3304
santzjr <121175663+santzjr@users.noreply.github.com>
2024-05-23 02:07:07 +0100

    Started practcicing with objects

---
+++ objects-practicse.cpp
@@ -0,0 +1,36 @@
+class MyClass {
+  public:
+    void myMethod() {
+      cout <<  "Hello World!";
+    }
+    int myNum;
+    string myString;
+};
+
+class Car {
+  public:
+    string brand; 
+    string model;
+    int year;
+}
+
+int main() {
+  
+  MyClass myObj;
+  myObj.myNum = 15;
+  myObj.myString= "Some text";
+  cout << myObj.num << "\n";
+
+  void myObj::myMethod(){
+     cout << "Hello World!"
+
+  Car carObj;
+  carObj.brand = "Ford";
+  carObj.model = "Mustang";
+  carObj.year = 1969;
+
+  cout << myObj.myString; 
+  cout << carObj.brand
+  
+  return 0;  
+}
